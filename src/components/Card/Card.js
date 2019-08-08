e1f4ffe5147c504823fdf1889a86e4823cf502ad
Konrad Kostrzanowski <konk99@o2.pl>
2019-08-08 18:00:10 +0200

    Refactor List and Column component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,19 @@
 import React from 'React';
 import styles from './Card.scss';
+import PropTypes from 'prop-types';
 
 class Card extends React.Component {
+    static propTypes = {
+      cards: PropTypes.node,
+      title: PropTypes.node,
+    }
+
     render() {
-        return (
-            <section className={styles.component}>
-                <h4>{this.props.title}</h4>
-            </section>
-        )
+      return (
+        <section className={styles.component}>
+          <h4>{this.props.title}</h4>
+        </section>
+      );
     }
 }
 
