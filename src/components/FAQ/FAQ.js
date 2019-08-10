fe5a78f0fe95abc4130207dcd525503400659c29
Konrad Kostrzanowski <konk99@o2.pl>
2019-08-10 15:17:21 +0200

    Add React Router

---
+++ src/components/FAQ/FAQ.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '../Container/Container';
+import Hero from '../Hero/Hero';
+
+const FAQ = () => (
+  <Container>
+    <Hero titleText='FAQ' image='src/images/faq.jpg' />
+    <p>Mauris maximus, purus at fringilla rutrum, metus tellus aliquam eros, convallis vestibulum nisi sapien sed turpis. Vestibulum in massa ac mauris dignissim lacinia. Aenean lacinia.</p>
+  </Container>
+);
+
+export default FAQ;
---
+++ src/components/Info/Info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '../Container/Container';
+import Hero from '../Hero/Hero';
+
+const Info = () => (
+  <Container>
+    <Hero titleText='Info' image='src/images/info.jpg' />
+    <p>Pellentesque in euismod risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec commodo vitae sem. </p>
+  </Container>
+);
+
+export default Info;
